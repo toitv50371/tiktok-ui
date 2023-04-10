27b9d8f30e6b82ef5b9883c27abe2d300d5225b4
Toi Tran <toitv.astary@gmail.com>
2023-04-10 16:18:56 +0700

    configuration Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
-  return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,13 @@
+import Header from "~/components/Layout/DefaultLayout/Header";
+function Upload() {
+    return (
+        <div>
+            <Header/>
+            <div className='content'>
+                <h2>Upload</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Upload;
