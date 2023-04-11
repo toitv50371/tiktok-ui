dcb4f68a96f4155742b2f0a12e73a1f00daaf34f
Toi Tran <toitv.astary@gmail.com>
2023-04-11 10:08:19 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,32 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,16 +1,19 @@
+import classNames from 'classnames/bind';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
 
-function DefaultLayout({children}) {
-    return ( 
-        <div>
-            <Header/>
-            <div className='container'>
-                <Sidebar/>  
-                <div className='content'>{children}</div>
+const cx = classNames.bind(styles);
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
-     )
+    );
 }
 
 export default DefaultLayout;
